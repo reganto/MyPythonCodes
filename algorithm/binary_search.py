572c4206db1725d5cad152b02256c0d52fe73047
Reganto <rreganto@gmail.com>
2022-03-06 23:06:45 +0330

    Iterative binary search

--- algorithm/binary_search.py
+++ algorithm/binary_search.py
@@ -1,21 +1,21 @@
-def binary_search(our_list, target):
-    first = 0
-    last = len(our_list) - 1
+import sys
 
+
+def binary_search(items, item):
+    first, last = 0, len(items) - 1
     while first <= last:
-        midpoint = (first + last) // 2
-
-        if target == our_list[midpoint]:
-            return midpoint
-        if target > our_list[midpoint]:
-            first = midpoint + 1
+        mid_point = (first + last) // 2  # find mid point index
+        if items[mid_point] == item:     # best case
+            return mid_point
+        elif items[mid_point] > item:
+            last = mid_point - 1
         else:
-            last = midpoint - 1
+            first = mid_point + 1
+    
+    # if item not exist in items
     return -1
 
+convert_to_list = lambda d: [int(number) for number in d[1: len(d)-1].split(', ')] 
+print(binary_search(convert_to_list(sys.argv[1]), int(sys.argv[2])))
 
-result = binary_search(
-    [number**2 for number in range(1, 11)],
-    81
-)
-print(result)
+# python app.py "[12, 34, 45, 90, 123, 231, 579, 1921]" 1921
